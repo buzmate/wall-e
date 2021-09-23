166370ced808a6c90d62b267c5deb0008ef0b34e
turinglabsorg <s.cataudo@turinglabs.org>
2021-09-23 14:52:11 +0200

    init commit

---
+++ generate.js
@@ -0,0 +1,72 @@
+const fs = require('fs')
+const argv = require('minimist')(process.argv.slice(2), { string: ["_"] });
+const Roll = require('roll')
+const roll = new Roll()
+
+async function main(instructions_file) {
+    if (fs.existsSync('./instructions/' + instructions_file)) {
+        if (!fs.existsSync('./results')) {
+            fs.mkdirSync('./results')
+        }
+        if (!fs.existsSync('./results/' + instructions_file)) {
+            fs.mkdirSync('./results/' + instructions_file)
+        }
+
+        const instructions = fs.readFileSync('./instructions/' + instructions_file).toString().split("\n")
+        console.log('Found ' + instructions.length + ' instructions!')
+
+        let to_generate = instructions[0].split(':')[1]
+        let traits = {}
+        console.log('Need to generate ' + to_generate + ' traits')
+        let generated = fs.readdirSync('./results/' + instructions_file)
+        if (generated.length < to_generate) {
+            for (let k in instructions) {
+                let instruction = instructions[k]
+                let skip = false
+                if (instruction.indexOf('GENERATE') !== -1) {
+                    skip = true
+                } else if (instruction.indexOf('#') === 0) {
+                    skip = true
+                }
+
+                if (!skip) {
+                    let trait_name = instruction.split('|')[1].split(':')[0].toLowerCase()
+                    let attribute = instruction.split('|')[1].split(':')[1].toLowerCase()
+                    let dice = instruction.split('|')[0].split('>')[0]
+                    let threshold = parseInt(instruction.split('|')[0].split('>')[1])
+
+                    if (traits[trait_name] === undefined) {
+                        console.log('-> TRAIT: ', trait_name)
+                        console.log('-> ATTRIBUTE:', attribute)
+                        console.log('--> SENDING ' + dice + ', pass if higher than ' + threshold)
+
+                        const dice_roll = roll.roll(dice)
+                        if (dice_roll.result > threshold) {
+                            console.log('--> NICE SHOT! ' + dice_roll.result + ' IS ENOUGH!', dice, '>', threshold)
+                            traits[trait_name] = attribute
+                        } else {
+                            console.log('--> BAD LUCK ' + dice_roll.result + ' ISN\'T ENOUGH!')
+                        }
+                    }
+                }
+            }
+
+            fs.writeFileSync('./results/' + instructions_file + '/' + generated.length + '.json', JSON.stringify(traits))
+
+            let next = generated.length++
+            if (next <= to_generate) {
+                main(instructions_file)
+            }
+        } else {
+            console.log('Generation ended!')
+        }
+    } else {
+        console.log('Sorry, ' + instructions_file + ' not found in folder `instructions`.')
+    }
+}
+
+if (argv._ !== undefined) {
+    main(argv._)
+} else {
+    console.log('Please give an instruction first')
+}
---
+++ stats.js
@@ -0,0 +1,52 @@
+const fs = require('fs')
+const argv = require('minimist')(process.argv.slice(2), { string: ["_"] });
+const Roll = require('roll')
+const roll = new Roll()
+
+async function main(instructions_file) {
+    if (fs.existsSync('./instructions/' + instructions_file)) {
+        if (fs.existsSync('./results/' + instructions_file)) {
+            let results = fs.readdirSync('./results/' + instructions_file)
+            console.log('Found ' + results.length + ' files to evaluate')
+            let counters = {}
+            for (let k in results) {
+                let traits = JSON.parse(fs.readFileSync('./results/' + instructions_file + '/' + results[k]))
+                for (let k in Object.keys(traits)) {
+                    let trait = Object.keys(traits)[k]
+                    let attribute = traits[trait]
+                    if (counters[trait] === undefined) {
+                        counters[trait] = {}
+                    }
+                    if (counters[trait][attribute] === undefined) {
+                        counters[trait][attribute] = 0
+                    }
+                    counters[trait][attribute]++
+                }
+            }
+            console.log('NUMERIC RESULT:')
+            console.log(counters)
+            console.log('--')
+            console.log('STATISTIC RESULT:')
+            for (let k in Object.keys(counters)) {
+                let trait = Object.keys(counters)[k]
+                console.log('| ' + trait + ' | ')
+                for (let j in counters[trait]) {
+                    let count = counters[trait][j]
+                    let percentage = 100 / results.length * count
+                    console.log(j + ': ' + percentage.toFixed(2) + '%')
+                }
+                console.log('--')
+            }
+        } else {
+            console.log('Nothing to evaluate.')
+        }
+    } else {
+        console.log('Sorry, ' + instructions_file + ' not found in folder `instructions`.')
+    }
+}
+
+if (argv._ !== undefined) {
+    main(argv._)
+} else {
+    console.log('Please give an instruction first')
+}
